c77b321eb9d8de6c4d3f98b4aaa534614f8b5f4b
lifekevin21 <lifekevin21@126.com>
2016-08-29 16:15:54 +0800

    processing

---
+++ pooled_processing.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 29 15:38:30 2016
+
+@author: kevin
+"""
+
+from multiprocessing import Pool
+import os, time, random
+
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f second.' % (name, (end - start)))
+
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args = (i, ))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
