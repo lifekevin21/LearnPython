368606a1edc4bffe1ae4398ef04f48266d9f16c5
lifekevin21 <lifekevin21@126.com>
2017-01-12 15:53:09 +0800

    fix error,function tcplink should be before the accept client module

--- do_tcp.py
+++ do_tcp.py
@@ -36,19 +36,6 @@ import threading
 import time
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-s.bind(('127.0.0.1', 9999))
-
-s.listen(5)
-print('Waiting for connection...')
-
-while True:
-    sock, addr = s.accept()
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
-
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
     sock.send(b'Welcome!')
@@ -60,3 +47,16 @@ def tcplink(sock, addr):
         sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind(('127.0.0.1', 9999))
+
+s.listen(5)
+print('Waiting for connection...')
+
+while True:
+    sock, addr = s.accept()
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
