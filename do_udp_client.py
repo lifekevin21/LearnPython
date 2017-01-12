7d04a5442a63ed7d2b8105f4f26956621901055f
lifekevin21 <lifekevin21@126.com>
2017-01-12 16:33:38 +0800

    add file udp

---
+++ do_udp_client.py
@@ -0,0 +1,13 @@
+# -*- encode:utf-8 -*-
+
+
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for data in [b'Kevin', b'Leona', b'Sam']:
+    s.sendto(data, ('127.0.0.1', 9999))
+    print(s.recv(1024).decode('utf-8'))
+
+s.close()
